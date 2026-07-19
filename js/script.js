2ddf011c06eb9085aa4229a398f8de660894bbd8
Manaton Games <manatongames@gmail.com>
2026-07-18 20:25:54 -0500

    Remove menu button click event listener

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-const menu = document.querySelector(".menu-btn");
 
-const links = document.querySelector(".nav-links");
-
-
-menu.addEventListener("click",()=>{
-
-
-links.classList.toggle("active");
-
-
-});
