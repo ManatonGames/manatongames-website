6515cf7bd1316df53c4d0c853ce40ac86b74139e
Manaton Games <manatongames@gmail.com>
2026-07-19 14:44:45 -0500

    Add API handler for Roblox game data

--- api/roblox.js
+++ api/roblox.js
@@ -1,1 +1,36 @@
+export default async function handler(req, res) {
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json");
+
+    return res.status(200).json({
+
+        success: true,
+
+        studio: "Manaton Games",
+
+        version: "2.0.0",
+
+        roblox: {
+
+            groupId: 15973191,
+
+            games: [
+
+                {
+                    id: 90485309557694,
+                    name: "PLS DONATE 3"
+                },
+
+                {
+                    id: 91290129805346,
+                    name: "+1 Speed Escape"
+                }
+
+            ]
+
+        }
+
+    });
+
+}
