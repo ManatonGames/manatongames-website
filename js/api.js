3c13c23819358dbdebf3d9ae3cd4c830adf2bd49
Manaton Games <manatongames@gmail.com>
2026-07-19 15:14:30 -0500

    Add RobloxAPI class with getStats method

--- js/api.js
+++ js/api.js
@@ -1,1 +1,23 @@
+class RobloxAPI {
 
+    static async getStats(){
+
+        try{
+
+            const response = await fetch("/api/roblox");
+
+            return await response.json();
+
+        }
+
+        catch(error){
+
+            console.error(error);
+
+            return null;
+
+        }
+
+    }
+
+}
