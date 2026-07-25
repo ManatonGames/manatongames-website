e0d866495e5fc1ec4cdc9a90674c06e2293e619e
Manaton Games <manatongames@gmail.com>
2026-07-25 15:26:16 -0500

    Implement Network First strategy in fetch handler
    Updated fetch event listener to implement a Network First strategy for handling requests.

--- sw.js
+++ sw.js
@@ -62,16 +62,28 @@ self.addEventListener("activate", event => {
 
 });
 
-// Obtener archivos
+// Obtener archivos (Network First)
 self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
 
     event.respondWith(
 
-        caches.match(event.request)
-
+        fetch(event.request)
             .then(response => {
 
-                return response || fetch(event.request);
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+
+            })
+            .catch(() => {
+
+                return caches.match(event.request);
 
             })
 
