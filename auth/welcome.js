349ad79f5e54e19c7b60391b419ec016af17ee20
Manaton Games <manatongames@gmail.com>
2026-07-28 19:22:39 -0500

    Load welcome component on DOMContentLoaded

--- auth/welcome.js
+++ auth/welcome.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    const container = document.getElementById("welcome-container");
+
+    const response = await fetch("components/welcome.html");
+
+    container.innerHTML = await response.text();
+
+});
