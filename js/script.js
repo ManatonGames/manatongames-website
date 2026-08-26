f8a1ebbe3087097dcd7a7f31251b1a6ea768299c
Manaton Games <manatongames@gmail.com>
2026-08-25 21:05:46 -0500

    Update condition to check for private WEBSITE_MODE

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     if (
         typeof WEBSITE_MODE !== "undefined" &&
-        WEBSITE_MODE === "public"
+        WEBSITE_MODE === "private"
     ) {
 
         console.log(
